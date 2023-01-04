33fc7f485c1666bf9f04e350e500c38bbe93b28e
JoseFlores25 <74205482+JoseFlores25@users.noreply.github.com>
2023-01-04 16:16:47 -0500

    Done

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -18,7 +18,7 @@ class BirdsController < ApplicationController
     if bird
       render json: bird
     else
-      render json: { error: "Bird not found" }, status: :not_found
+      render_not_found_response
     end
   end
 
@@ -29,7 +29,7 @@ class BirdsController < ApplicationController
       bird.update(bird_params)
       render json: bird
     else
-      render json: { error: "Bird not found" }, status: :not_found
+      render_not_found_response
     end
   end
 
@@ -40,7 +40,7 @@ class BirdsController < ApplicationController
       bird.update(likes: bird.likes + 1)
       render json: bird
     else
-      render json: { error: "Bird not found" }, status: :not_found
+      render_not_found_response
     end
   end
 
@@ -51,7 +51,7 @@ class BirdsController < ApplicationController
       bird.destroy
       head :no_content
     else
-      render json: { error: "Bird not found" }, status: :not_found
+      render_not_found_response
     end
   end
 
